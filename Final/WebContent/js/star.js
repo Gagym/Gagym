c9d7a0378641ddff26cf9c5943b02148d4669d5a
DESKTOP-8L0S02D\Song <shd2768@naver.com>
2022-02-15 11:59:33 +0900

    테스트

--- Final/WebContent/js/star.js
+++
@@ -1,8 +0,0 @@
-const drawStar = (target) => {
-    document.querySelector(`.star span`).style.width = `${target.value * 20}%`;
-}
-
-const showStar = () => {
-	let target = document.querySelector(`#grade`);
-    document.querySelector(`.star span`).style.width = `${target.value * 20}%`;
-}
